e2e2ec676139b5b9be2c731266d2e5c21ab0a773
Wind-2375-like <yiyang.feng.2375@gmail.com>
2023-06-13 21:22:25 +0200

    fixed a bug in ppo_trainer; but there's still other bugs

--- ppo_trainer.py
+++ ppo_trainer.py
@@ -1,5 +1,5 @@
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 import torch
 from torch.utils.data import Dataset, DataLoader
 from transformers import GPT2Tokenizer, GPT2LMHeadModel, GPT2ForSequenceClassification
@@ -11,14 +11,14 @@ import random
 
 # Argument parser for hyperparameters
 parser = argparse.ArgumentParser()
-parser.add_argument('--ppo_epochs', type=int, default=256000, help='Number of PPO epochs')
+parser.add_argument('--ppo_epochs', type=int, default=50, help='Number of PPO epochs')
 parser.add_argument('--lr', type=float, default=9e-6, help='Learning rate')
 parser.add_argument('--beta', type=float, default=0.02, help='Beta for KL penalty')
 # parser.add_argument('--batch_size', type=int, default=512, help='Batch size')
-parser.add_argument('--batch_size', type=int, default=4, help='Batch size')
+parser.add_argument('--batch_size', type=int, default=8, help='Batch size')
 parser.add_argument('--ppo_clip_ratio', type=float, default=0.2, help='PPO clip ratio')
 # parser.add_argument('--mini_batch_size', type=int, default=64, help='Mini-batch size')
-parser.add_argument('--mini_batch_size', type=int, default=1, help='Mini-batch size')
+parser.add_argument('--mini_batch_size', type=int, default=4, help='Mini-batch size')
 parser.add_argument('--seed', type=int, default=42, help='Random seed')
 args = parser.parse_args()
 
@@ -31,7 +31,6 @@ if torch.cuda.is_available():
 
 # Set the device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# device = torch.device("cpu")
 
 # Load the pre-trained reward model
 reward_model = GPT2ForSequenceClassification.from_pretrained("models/reward_model", num_labels=1).to(device)
@@ -70,6 +69,7 @@ def generate_text(model, instruction, max_length=1023):
     inputs = encoded_inputs['input_ids'].to(device)
     attention_mask = encoded_inputs['attention_mask'].to(device)
     outputs = model.module.generate(inputs, attention_mask=attention_mask, max_length=max_length, do_sample=True, pad_token_id=tokenizer.pad_token_id, eos_token_id = tokenizer.eos_token_id, temperature=1.0)
+    # outputs = model.generate(inputs, attention_mask=attention_mask, max_length=max_length, do_sample=True, pad_token_id=tokenizer.pad_token_id, eos_token_id = tokenizer.eos_token_id, temperature=1.0)
     return [inputs, outputs]
 
 
@@ -90,6 +90,7 @@ def get_log_prob(model, instruction_ids, interaction_ids):
 
     # Get the output logits from the model
     outputs = model.module(interaction_ids)
+    # outputs = model(interaction_ids)
     logits = outputs.logits[:, :-1, :]  # Exclude the last token
 
     # Get the softmax probabilities from the logits
@@ -161,68 +162,74 @@ def calculate_rewards(model, interactions):
     # Pad all interactions to 1024.
     interactions = pad_sequence(interactions, max_length=1024, padding_value=tokenizer.pad_token_id)
     with torch.no_grad():
-        rewards = model(interactions).logits.squeeze()
+        rewards = model.module(interactions).logits.squeeze()
     return rewards
 
 
-def ppo_iter(mini_batch_size, new_log_probs, old_log_probs, advantage):
+def ppo_iter(mini_batch_size, instructions_interactions, advantage):
+    '''
+    Generate mini batches for PPO update
+    Input:
+        mini_batch_size: int
+        instructions_interactions: list of tuple of torch.tensor, the token ids of the instructions and interactions. The length of the list is the batch size
+        advantage: torch.tensor, the advantage of the interactions, the shape is (batch_size, 1)
+    Output:
+        tuple of two lists and one torch.tensor, the token ids of the instructions, interactions and the advantage
+    '''
     batch_size = advantage.size(0)
     ids = np.random.permutation(batch_size)
     ids = ids[:mini_batch_size * (batch_size // mini_batch_size)]
 
-    for i in range(batch_size // mini_batch_size):
-        yield\
-            new_log_probs[ids[i * mini_batch_size:(i + 1) * mini_batch_size]],\
-            old_log_probs[ids[i * mini_batch_size:(i + 1) * mini_batch_size]],\
-            advantage[ids[i * mini_batch_size:(i + 1) * mini_batch_size]]
+    for i in range(0, len(ids), mini_batch_size):
+        batch_ids = ids[i:i + mini_batch_size]
+        yield [instructions_interactions[i][0] for i in batch_ids], [instructions_interactions[i][1] for i in batch_ids], advantage[batch_ids]
 
 
-def ppo_update(ppo_epochs, mini_batch_size, ppo_log_probs, sft_log_probs, advantages, clip_param=args.ppo_clip_ratio):
-    for _ in range(ppo_epochs):
-        for new_log_probs, old_log_probs, advantage in ppo_iter(mini_batch_size, ppo_log_probs, sft_log_probs, advantages):
-            optimizer.zero_grad()
+def ppo_update(mini_batch_size, instructions_interactions, advantages, clip_param=args.ppo_clip_ratio):
+    for instructions, interactions, advantage in ppo_iter(mini_batch_size, instructions_interactions, advantages):
+        
+        # Calculate the log probability of the generated demonstrations
+        ppo_log_probs = [get_log_prob(ppo_model, inst, inter) for inst, inter in zip(instructions, interactions)]
+        with torch.no_grad():
+            sft_log_probs = [get_log_prob(sft_model, inst, inter) for inst, inter in zip(instructions, interactions)]
             
-            ratio = (new_log_probs - old_log_probs).exp()
-            # Normalize the advantage
-            advantage = (advantage - advantage.mean()) / (advantage.std() + 1e-5)
-            
-            surr1 = ratio * advantage
-            surr2 = torch.clamp(ratio, 1.0 - clip_param, 1.0 + clip_param) * advantage
+        # Convert log probabilities to tensors
+        new_log_probs = torch.stack(ppo_log_probs)
+        old_log_probs = torch.stack(sft_log_probs)
+        old_log_probs = old_log_probs.detach()
+        
+        ratio = (new_log_probs - old_log_probs).exp()
+        
+        surr1 = ratio * advantage
+        surr2 = torch.clamp(ratio, 1.0 - clip_param, 1.0 + clip_param) * advantage
 
-            loss  = - torch.min(surr1, surr2).mean()
-            
-            loss.backward()
-            optimizer.step()
+        loss  = - torch.min(surr1, surr2).mean()
+        optimizer.zero_grad()
+        
+        loss.backward()
+        optimizer.step()
 
 
 optimizer = torch.optim.Adam(ppo_model.parameters(), lr=args.lr)
 
 best_reward = -float('inf')
-for epoch in tqdm(range(args.ppo_epochs)):
-    for instructions in train_dataloader:
+for epoch in range(args.ppo_epochs):
+    for instructions in tqdm(train_dataloader):
         # Generate demonstrations from the ppo model given the input instruction
         # instructions_interactions is a list of [instruction_ids, interaction_ids]
-        instructions_interactions = [generate_text(ppo_model, i) for i in instructions]
-        instructions = [i[0] for i in instructions_interactions]
-        interactions = [i[1] for i in instructions_interactions]
+        with torch.no_grad():
+            instructions_interactions = [generate_text(ppo_model, i) for i in instructions]
+            interactions = [i[1] for i in instructions_interactions]
 
-        # Calculate the reward given interactions
-        rewards = calculate_rewards(reward_model, interactions)
+            # Calculate the reward given interactions
+            rewards = calculate_rewards(reward_model, interactions)
 
         # Calculate the advantages
         advantages = rewards - rewards.mean()
-
-        # Calculate the log probability of the generated demonstrations
-        ppo_log_probs = [get_log_prob(ppo_model, i[0], i[1]) for i in instructions_interactions]
-        with torch.no_grad():
-            sft_log_probs = [get_log_prob(sft_model, i[0], i[1]) for i in instructions_interactions]
-            
-        # Convert log probabilities to tensors
-        ppo_log_probs = torch.stack(ppo_log_probs)
-        sft_log_probs = torch.stack(sft_log_probs)
+        advantages = advantages.reshape(-1, 1)
 
         # Update the model
-        ppo_update(args.ppo_epochs, args.mini_batch_size, ppo_log_probs, sft_log_probs, advantages)
+        ppo_update(args.mini_batch_size, instructions_interactions, advantages)
         
     # Evaluation loop
     with torch.no_grad():
@@ -231,7 +238,6 @@ for epoch in tqdm(range(args.ppo_epochs)):
         for instruction in val_dataloader:
             # Generate demonstrations from the ppo model given the input instruction
             instructions_interactions = [generate_text(ppo_model, i) for i in instructions]
-            instructions = [i[0] for i in instructions_interactions]
             interactions = [i[1] for i in instructions_interactions]
             
             # Calculate the reward given interactions
@@ -240,7 +246,7 @@ for epoch in tqdm(range(args.ppo_epochs)):
             avg_reward += rewards.mean().item()
             num_batches += 1
         avg_reward /= num_batches
-        print('Average reward: {}'.format(avg_reward))
+        print('Epoch {}, average reward {}'.format(epoch, avg_reward))
         
         # Save the model if the average reward is higher than the previous best
         if avg_reward > best_reward:
